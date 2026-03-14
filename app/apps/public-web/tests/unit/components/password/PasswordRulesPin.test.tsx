62cec790572ebee2da5f95cb079bb801fc983710
Oskar Wichtowski <oskar.wichtowski3@gmail.com>
2026-03-14 22:07:41 +0100

    fix

--- app/apps/public-web/tests/unit/components/password/PasswordRulesPin.test.tsx
+++ app/apps/public-web/tests/unit/components/password/PasswordRulesPin.test.tsx
@@ -1,7 +1,5 @@
 import "@testing-library/jest-dom";
-
 import { render, screen } from "@testing-library/react";
-import React from "react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 
 import { PasswordRulesPin } from "../../../../src/components/password/RulesPin";
